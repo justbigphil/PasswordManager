074438a5a30bc407049790f8e26f722ea9412f15
Phil <tr1ed72@hotmail.co.uk>
2020-12-14 13:17:46 +0000

    Test

--- PasswordManager/src/application/Main.java
+++ PasswordManager/src/application/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Password Manager");
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
